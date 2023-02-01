9a56e97e2ed4476b2e6099cbca7a5249ae7810bd
Shirin Pavel <trqktop@yandex.ru>
2023-02-01 19:57:54 +0300

    ghpages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.scss';
-import { cardAnimationConfig } from './js/constants/constants'
-import enableCardsAnimation from './js/components/enableCardsAnimation'
-
-enableCardsAnimation(cardAnimationConfig)
-
-
